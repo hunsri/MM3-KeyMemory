960629846bffc3a54deba07267e1f94cedca8b3b
phnyn <nguyen_thi_phuong@live.de>
2022-01-19 13:38:26 +0100

    Added the pianoroll (checkPixel and layout not working properly yet)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { PianoRoll } from './PianoRoll/PianoRoll';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <PianoRoll />
   </React.StrictMode>,
   document.getElementById('root'),
 );
